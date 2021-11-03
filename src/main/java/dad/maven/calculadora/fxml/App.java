8f716bd6d26708c81df998284be1b9518941614f
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2021-11-03 09:57:14 +0000

    Update App.java

--- src/main/java/dad/maven/calculadora/fxml/App.java
+++ src/main/java/dad/maven/calculadora/fxml/App.java
@@ -1,18 +1,27 @@
 package dad.maven.calculadora.fxml;
-
+ 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+	private Controller controller;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		controller = new Controller();
 
+		Scene scena = new Scene(controller.getView());
+		
+		primaryStage.setTitle("Calculadora - FXML");
+		primaryStage.setScene(scena);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 
 }
