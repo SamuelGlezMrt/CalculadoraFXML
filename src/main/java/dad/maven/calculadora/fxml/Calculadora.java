e2c6e474c028fa0135b3579d58c6772f8a981ba3
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2021-11-03 09:57:17 +0000

    Update Calculadora.java

--- src/main/java/dad/maven/calculadora/fxml/Calculadora.java
+++ src/main/java/dad/maven/calculadora/fxml/Calculadora.java
@@ -1,5 +1,8 @@
 package dad.maven.calculadora.fxml;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Implementaci�n de la l�gica de una calculadora.
  * @author Fran Vargas
@@ -17,7 +20,7 @@ public class Calculadora {
 	private Double operando;
 	private char operador;
 	private Boolean nuevoOperando;
-	private String pantalla;
+	private StringProperty pantalla = new SimpleStringProperty();
 	
 	public Calculadora() {
 		borrar();
@@ -27,8 +30,13 @@ public class Calculadora {
 	 * Devuelve el contenido de la pantalla de la calculadora.
 	 * @return Cadena de texto con el contenido de la pantalla de la calculdora.
 	 */
-	public String getPantalla() {
-		return pantalla;
+	public final StringProperty pantallaProperty() {
+		return this.pantalla;
+	}
+	
+
+	public final String getPantalla() {
+		return this.pantallaProperty().get();
 	}
 
 	/**
@@ -45,7 +53,7 @@ public class Calculadora {
 	 */
 	public void borrarTodo() {
 		nuevoOperando = true;
-		pantalla = "0.0";
+		pantalla.set("0.0"); 
 	}
 	
 	/**
@@ -54,7 +62,7 @@ public class Calculadora {
 	 */
 	public void operar(char operador) {
 		nuevoOperando = true;
-		double operando2 = Double.parseDouble(pantalla);
+		double operando2 = Double.parseDouble(pantalla.get());
 		switch (this.operador) {
 			case SUMAR: operando += operando2; break;
 			case RESTAR: operando -= operando2; break;
@@ -63,15 +71,15 @@ public class Calculadora {
 			case IGUAL: operando = operando2; break;
 		}
 		this.operador = operador;
-		pantalla = "" + operando;
+		pantalla.set(""+operando); 
 	}
 	
 	/**
 	 * Inserta una coma en el operando actual (pantalla).
 	 */
 	public void insertarComa() {
-		if (!pantalla.contains("" + COMA)) {
-			pantalla += COMA;
+		if (!pantalla.get().contains("" + COMA)) {
+			pantalla.set(pantalla.get()+COMA);
 		}
 	}
 	
@@ -83,9 +91,9 @@ public class Calculadora {
 		if (digito >= '0' && digito <= '9') {
 			if (nuevoOperando) {
 				nuevoOperando = false;
-				pantalla = "";
+				pantalla.set("");
 			}
-			pantalla += digito;
+			pantalla.set(pantalla.get() + digito);
 		} else if (digito == COMA) {
 			insertarComa();
 		}
